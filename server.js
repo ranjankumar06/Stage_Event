2307ca2ea045a1e7e91c5f0ac627db1d28578158
raviprarai <raviprarai@gmail.com>
2022-08-16 16:50:50 +0530

    New data

--- server.js
+++ server.js
@@ -5,17 +5,18 @@ const app= express();
 const swaggerUi = require("swagger-ui-express");
 const swaggerJSDoc = require("swagger-jsdoc");
 require('./dbConnection/db');
-const PORT = 3003;
+const PORT = 3007;
 app.use(express.json())
 app.use(express.urlencoded({ extended: false}))
 app.use(bodyParser.urlencoded({ extended: true}))
 app.use(bodyParser.json())
+// app.use(cors({origin: 'http://localhost:3000'}));
+
 app.use('/user',require('./Router/userRouter'))
 app.use('/admin',require('./Router/adminRouter'))
 app.use('/organizer',require('./Router/organizerRouter'))
 app.use('/event',require('./Router/eventRouter'))
 app.use('/ticket',require('./Router/ticketRouter'))
-app.use(cors());
 
 
 
@@ -43,18 +44,23 @@ const swaggerDefinition = {
    app.use("/Ravi-API", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 //    Allow cors
 
-app.use(function(req,res,next){
-    res.header("Access-Control-Allow-Origin","*");
-    res.header(
-        "Allow-Control-Allow-Headers",
-        "Origin,X-Requested-With,Content-Type,Accept,Authorization"
-    );
-    res.header('Access-Control-Allow-Methods','GET,PUT,PATCH,POST,DELETE');
+// app.use(function(req,res,next){
+//     res.header("Access-Control-Allow-Origin","*");
+//     res.header(
+//         "Allow-Control-Allow-Headers",
+//         "Origin,X-Requested-With,Content-Type,Accept,Authorization"
+//     );
+//     res.header('Access-Control-Allow-Methods','GET,PUT,PATCH,POST,DELETE');
+//     next();
+// });
+var corsOptions = {
+    origin: 'http://localhost:3000',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  }
+    app.get('/data',cors(corsOptions),function(req, res,next){
+        res.json({msg: 'This is CORS-enabled for only example.com.'})
     next();
-});
 
-    app.get('/data',function(req, res){
-    res.send(`Hi my name is RAVI`);
 })
 app.listen(PORT,(err,res)=>{
     if (err) {
