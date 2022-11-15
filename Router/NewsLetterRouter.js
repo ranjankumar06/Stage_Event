13c491befd622a1629358035fa5230e3a11099d5
mukeshyadavbca777 <sonuyadavbca777@gmail.com>
2022-11-15 15:48:11 +0530

    add emailsearch api in news letter

--- Router/NewsLetterRouter.js
+++ Router/NewsLetterRouter.js
@@ -5,4 +5,7 @@ router.post('/addSubscription',newsletter.addSubscription)
 
 router.get('/getAllUserSubscription',newsletter.getAllUserSubscription)
 
+router.get('/EmailSearch',newsletter.EmailSearch)
+
+
 module.exports=router
