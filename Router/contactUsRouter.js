364d02478f3733d83d08f34def2af1cf3341598e
mukeshyadavbca777 <sonuyadavbca777@gmail.com>
2022-11-17 12:21:56 +0530

    add getcontectbyid api

--- Router/contactUsRouter.js
+++ Router/contactUsRouter.js
@@ -3,6 +3,7 @@ const contactUsRouter = require('../controller/contactUsController')
 router.post("/addContact",contactUsRouter.addContact)
 router.get("/allContact",contactUsRouter.allContact)
 router.get("/subjectSearch",contactUsRouter.subjectSearch)
+router.get('/getContectById/:id',contactUsRouter.getContectById)
 
 
 
